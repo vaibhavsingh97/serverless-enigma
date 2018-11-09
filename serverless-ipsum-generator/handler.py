cc6ce932ef009cb6458eb0a93c902e27595f633d
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-11-09 20:14:36 +0530

    fixed cors error

--- serverless-ipsum-generator/handler.py
+++ serverless-ipsum-generator/handler.py
@@ -8,6 +8,10 @@ def impsum_generator(event, context):
     fake = Faker()
     response = {
         "statusCode": 200,
+        "headers": {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : "true"
+        },
         "body": str(fake.sentence(210, ext_word_list=word_list))
     }
 
